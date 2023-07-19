29ebf73559099633380b0765533c8e16b4d5222e
Savio Viana <savios138@gmail.com>
2023-07-19 10:10:06 -0300

    Ocean-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import Card from './components/Card/Card'
 
+export const item1 = {
+  nome: "Rick Sanchez",
+  imagem:
+    "https://static.tvtropes.org/pmwiki/pub/images/abcb6534_7913_4eb1_a7a5_62b081ebc628.png"
+}
+
+export const item2 = {
+  nome: "Morty Smith",
+  imagem:
+    "https://static.wikia.nocookie.net/rickandmorty/images/e/ee/Morty501.png/revision/latest?cb=20210827150137"
+}
 
 function App() {
   return (
     <>
       <div className='cards-list'>
-        <Card />
-        <Card />
-        <Card />
+        <Card item={1} />
+        <Card item={2}/>
       </div>
     </>
   )
