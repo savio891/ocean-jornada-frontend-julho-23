46f97770282b962c25600a83f1fce2b8fd135e69
Savio Viana <savios138@gmail.com>
2023-07-19 12:01:53 -0300

    Ocean-project

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,9 @@
 import "./Card.css";
 
-export default function Card(props) {
-  // console.log(props);
+export default function Card(props) {//Lê os atributos que estão no componente Card.
+  //console.log(props);
 
-  const item = props.item;
+  const item = props.item; //corresponde ao atributo que armazena as variáveis item1 e item2.
 
   // console.log(item);
 
